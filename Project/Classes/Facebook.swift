836c949f7c4abb65b47c5ae7e94dd062c9476088
Alexander Rinne <Alex@wcw-stud-145-109-56-251.wireless.uva.nl>
2017-06-13 16:00:02 +0200

    Facebook login werkt

---
+++ Project/Classes/Facebook.swift
@@ -0,0 +1,16 @@
+//
+//  Facebook.swift
+//  Project
+//
+//  Created by Alexander Rinne on 13-06-17.
+//  Copyright © 2017 Alexander Rinne. All rights reserved.
+//
+
+import Foundation
+
+class Facebook {
+    
+    
+    
+    static let sharedInstance = Facebook()
+}
--- Project/LogInViewController.swift
+++ Project/LogInViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
     
@@ -25,8 +26,27 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let loginButton = FBSDKLoginButton()
+        
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginButton)
+        
+        let horizontalConstraint = NSLayoutConstraint(item: loginButton, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: passwordTextField, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+        let verticalConstraint = NSLayoutConstraint(item: loginButton, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: passwordTextField, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 40)
+        let widthConstraint = NSLayoutConstraint(item: loginButton, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 185)
+        let heightConstraint = NSLayoutConstraint(item: loginButton, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 25)
+       view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+        
+        //        let FBSDLoginManager.sharedInstance
+        
+        
+//        if FBSDKAccessToken == currentAccessToken {
+//            performSegue(withIdentifier: "loginToMyPlaces", sender: nil)
+//        }
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
